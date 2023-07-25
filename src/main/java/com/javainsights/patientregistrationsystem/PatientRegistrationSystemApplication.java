c0e0fa4acf3f026b71fee4aaa190eb503cad229b
Babu Perumal <babuitsmyofficial@gmail.com>
2023-07-25 15:23:34 +0530

    Initial commit

---
+++ src/main/java/com/javainsights/patientregistrationsystem/PatientRegistrationSystemApplication.java
@@ -0,0 +1,13 @@
+package com.javainsights.patientregistrationsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PatientRegistrationSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PatientRegistrationSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/javainsights/patientregistrationsystem/PatientRegistrationSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javainsights.patientregistrationsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PatientRegistrationSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
